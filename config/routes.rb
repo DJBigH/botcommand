c03e0678faf491779ed5151369fb7e5899903bbb
bigh0405 <khanhbeotixiu8@gmail.com>
2025-07-11 16:01:07 +0700

    Add action search ,hot, price

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,17 @@ Rails.application.routes.draw do
   # config/routes.rb
   get '/bots/:id/live-chat', to: 'bots#chat', as: 'bot_chat_ui'
   post '/bots/:id/chat',      to: 'bots#chat_response', as: 'bot_chat'
+  
+  namespace :api do
+  resources :categories
+  resources :products do
+    collection do
+      get :hot
+      get :by_category
+      get :search
+      get :by_price_range
+    end
+  end
+  end
+
 end
